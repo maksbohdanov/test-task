7f0ed8cbd44e43a96369b98eba151c85216ab0e3
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2023-02-02 12:26:47 +0200

    Create a client for calling OpenAPIs

---
+++ RickAndMortyApi/Infrastructure.Clients/IHttpClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Clients
+{
+    public interface IHttpClient
+    {
+        public HttpClient HttpClient { get; set; }
+        public string Url { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ RickAndMortyApi/Infrastructure.Clients/RickAndMortyClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace Infrastructure.Clients
+{
+    public class RickAndMortyClient: IHttpClient
+    {
+        public HttpClient HttpClient { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+
+        public RickAndMortyClient()
+        {
+            Url = "https://rickandmortyapi.com/api";
+            HttpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(Url)
+            };
+            SetDefaultHeaders();
+        }
+
+        private void SetDefaultHeaders()
+        {
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
