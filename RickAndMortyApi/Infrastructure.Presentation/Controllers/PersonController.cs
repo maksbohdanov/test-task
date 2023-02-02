0d669e5ae42a12c986f8b63dd2764f796ac4038c
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2023-02-02 16:19:34 +0200

    Create Person controller

---
+++ RickAndMortyApi/Infrastructure.Presentation/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using Core.Contracts.Models;
+using Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infrastructure.Presentation.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IConverterService _converterService;
+
+        public PersonController(IConverterService converterService) 
+        {
+            _converterService = converterService;
+        }
+
+        [HttpGet("person")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<CharacterModel>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> GetByName([FromQuery] string name)
+        {
+            var characters = await _converterService.GetCharactersByName(name);
+
+            return Ok(characters);
+        }
+
+        [HttpPost("check-person")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> CheckPersonInEpisode([FromBody] PresenceRequest model)
+        {
+            throw new NotImplementedException();
+            //var result = await _converterService.GetCharactersByName(name);
+
+            //return Ok(result);
+        }
+    }
+}
