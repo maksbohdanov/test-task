0f47f635e4be1c0cd8aab78da3e1b732cd1d07c6
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2023-02-02 16:18:05 +0200

    Implement ConverterService

---
+++ RickAndMortyApi/Core.Services.Abstractions/IConverterService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Contracts.Models;
+
+namespace Core.Services.Abstractions
+{
+    public interface IConverterService
+    {
+        //
+        Task<IEnumerable<CharacterModel>> GetCharactersByName(string name);
+    }
+}
---
+++ RickAndMortyApi/Core.Services/ConverterService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Core.Contracts.Models;
+using Core.Domain.Entities;
+using Core.Domain.Exceptions;
+using Core.Services.Abstractions;
+
+namespace Core.Services
+{
+    public class ConverterService : IConverterService
+    {
+        private readonly ICharacterService _characterService;
+        private readonly ILocationService _locationService;
+        private readonly IEpisodeService _episodeService;
+        private readonly IMapper _mapper;
+
+        public ConverterService(ICharacterService characterService, ILocationService locationService,
+            IEpisodeService episodeService, IMapper mapper)
+        {
+            _characterService = characterService;
+            _locationService = locationService;
+            _episodeService = episodeService;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CharacterModel>> GetCharactersByName(string name)
+        {
+            var characters = await _characterService.GetCharactersByName(name);
+            if(characters != null  && characters.Any())
+            {
+                foreach (var character in characters)
+                {
+                    var url = character.Origin?.Url;
+                    character.Origin = await GetLocationByUrl(url);
+                }
+
+                return _mapper.Map<IEnumerable<CharacterModel>>(characters);
+            }
+            else
+                throw new NotFoundException("Character with specified name was not found.");
+
+        }
+
+        private async Task<Location?> GetLocationByUrl(string? url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return new Location();
+
+            var locationId = url.Substring(url.LastIndexOf('/'));
+            return await _locationService.GetLocationById(locationId);
+        }
+    }
+}
